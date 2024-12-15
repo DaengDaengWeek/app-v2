9c22b4ee65a157ae5dbb7a69c573533d3906ff3b
Jini <yjkwon424@gmail.com>
2024-12-15 23:40:30 +0900

    Feat: 서약서 모달 뷰 UI 틀 생성

--- DaengDaengWeek/Views/StartScene/PledgeModalView.swift
+++ DaengDaengWeek/Views/StartScene/PledgeModalView.swift
@@ -5,6 +5,35 @@
 //  Created by Jini on 11/25/24.
 //
 
-import Foundation
+import SwiftUI
 
-// 서약서 모달 뷰
+struct PledgeModalView: View {
+    
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+        
+        ZStack {
+            pledgeview
+        }
+ 
+    }
+    
+    var pledgeview: some View {
+        ZStack {
+            HStack {
+                Spacer()
+                
+                Button(action: {
+                    isPresented = false
+                }) {
+                    Image("closeBtn")
+                        .resizable()
+                        .frame(width: 51, height: 51)
+                }
+                .offset(x: -35, y: -75)
+            }
+        }
+        .frame(width: 329, height: 689)
+    }
+}

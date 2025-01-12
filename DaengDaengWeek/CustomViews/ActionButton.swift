4901cb0b79f335d89050356e4b61168765683767
Jini <yjkwon424@gmail.com>
2025-01-12 23:15:51 +0900

    Feat: 하단 액션 버튼 컴포넌트 생성

--- DaengDaengWeek/CustomViews/ActionButton.swift
+++ DaengDaengWeek/CustomViews/ActionButton.swift
@@ -10,11 +10,42 @@ import SwiftUI
 // 메인, 병원 하단의 상호작용 버튼
 
 struct ActionButton: View {
+    
+    var label: String
+    var icon: String
+    // var size: Int // 아이콘 사이즈 재조정 필요하면 사용
+    var action: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: action) {
+            ZStack {
+                // 버튼 배경
+                Color.dwBeige10
+                    .clipShape(RoundedRectangle(cornerRadius: 24))
+                
+                // 아이콘, 텍스트
+                VStack(spacing: 2) {
+                    Image(icon)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 56, height: 56)
+                    
+                    Text(label)
+                        .font(.dw(.light, size: 13))
+                        .foregroundColor(.dwBrown00)
+                }
+                
+            }
+            .frame(width: 82, height: 82)
+            .overlay(
+                RoundedRectangle(cornerRadius: 24)
+                    .stroke(Color.dwBrown00, lineWidth: 2)
+                    .frame(width: 82, height: 82)
+            )
+        }
     }
 }
 
 #Preview {
-    ActionButton()
+    ActionButton(label: "먹이주기", icon: "feedIcon", action: {})
 }
--- DaengDaengWeek/Extensions/Color.swift
+++ DaengDaengWeek/Extensions/Color.swift
@@ -29,16 +29,26 @@ extension Color {
     
     // 16진수 색상코드 가져와서 커스텀 컬러 지정
     static let dwBeige00 = Color(hex: "#FAF5EB")
-    static let dwBeige10 = Color(hex: "#F0EADD")
+    static let dwBeige10 = Color(hex: "#F8EBDB")
     static let dwBeige20 = Color(hex: "#F1E9D7")
+    static let dwBeige30 = Color(hex: "#F0EADD")
     
     static let dwBrown00 = Color(hex: "#815C56")
+    static let dwBrown10 = Color(hex: "#B0958C")
+    static let dwBrown20 = Color(hex: "#B0A9A9")
     
-    static let dwPink = Color(hex: "FFA6EF")
+    static let dwPink = Color(hex: "#FFA6EF")
+    
+    static let dwWhite = Color(hex: "#FFFFFF")
+    static let dwBlack = Color(hex: "#000000")
     
     static let dwMint00 = Color(hex: "#86D1D1")
     static let dwMint10 = Color(hex: "#59C0C0")
     static let dwMint20 = Color(hex: "#00A3A3")
+    
+    static let dwGray00 = Color(hex: "#C7C7CC")
+    static let dwGray10 = Color(hex: "#8E8E93")
+    static let dwGray20 = Color(hex: "#F0F0F0")
 
 }
 

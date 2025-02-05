bd96febd16ff56243799a4d10d0571966b25daa7
Jini <yjkwon424@gmail.com>
2025-02-05 18:10:46 +0900

    Feat: 산책 시간 측정 타이머 추가

---
+++ DaengDaengWeek/CustomViews/WalkTimerView.swift
@@ -0,0 +1,60 @@
+//
+//  WalkTimerView.swift
+//  DaengDaengWeek
+//
+//  Created by Jini on 2/5/25.
+//
+
+import SwiftUI
+import Combine
+
+struct WalkTimerView: View {
+    
+    @ObservedObject var walkTimer = WalkTimer()
+    
+    var body: some View {
+        HStack(spacing: 4) {
+            Text("\(String(format: "%02d", walkTimer.minute))")
+                .font(.dw(.bold, size: 32))
+                .foregroundColor(.dwBrown00)
+                .frame(width: 55, alignment: .center)
+                .padding(.leading, 10)
+            
+            Text(":")
+                .font(.dw(.bold, size: 32))
+                .foregroundColor(.dwBrown00)
+                .frame(width: 8, alignment: .center)
+            
+            Text("\(String(format: "%02d", walkTimer.second))")
+                .font(.dw(.bold, size: 32))
+                .foregroundColor(.dwBrown00)
+                .frame(width: 55, alignment: .center)
+                .padding(.trailing, 20)
+        }
+    }
+    
+    
+}
+
+#Preview {
+    WalkView()
+}
+
+class WalkTimer: ObservableObject {
+    
+    @Published var second: Int = 0
+    @Published var minute: Int = 0
+    
+    init() {
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            self.second += 1
+            
+            if self.second == 60 {
+                self.second = 0
+                self.minute += 1
+            }
+        }
+    }
+    
+    
+}
---
+++ DaengDaengWeek/Views/ParkScene/WalkStartModalView.swift
@@ -0,0 +1,8 @@
+//
+//  WalkStartModalView.swift
+//  DaengDaengWeek
+//
+//  Created by Jini on 2/5/25.
+//
+
+import Foundation
